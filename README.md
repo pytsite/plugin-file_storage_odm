84207f3c5de291c89ab9d7260e44ece6e18448a5
Alexander Shepetko <a@shepetko.com>
2017-11-25 19:42:03 +0200

    Release 1.0

---
+++ README.md
@@ -0,0 +1,9 @@
+# PytSite ODM File Storage Plugin
+
+
+## Changelog
+
+
+### 1.0 (2017-11-25)
+
+First release.
---
+++ __init__.py
@@ -0,0 +1,27 @@
+"""PytSite ODM File Storage.
+"""
+from . import _model as model, _field as field
+from ._driver import Driver
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+def _init():
+    from pytsite import router, update
+    from plugins import odm
+    from . import _model, _eh, _controllers
+
+    # Register ODM models
+    odm.register_model('file', _model.AnyFileODMEntity)
+    odm.register_model('file_image', _model.ImageFileODMEntity)
+
+    # Event handlers
+    update.on_update(_eh.update)
+
+    router.handle(_controllers.Image(), '/image/resize/<int:width>/<int:height>/<p1>/<p2>/<filename>',
+                  'file_storage_odm@image', defaults={'width': 0, 'height': 0})
+
+
+_init()
